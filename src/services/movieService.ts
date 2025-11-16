e409294cecf8d124c2bcebf3eed69cd29a07f207
Oleg <luchkoo22@gmail.com>
2025-11-16 10:31:15 +0200

    edit

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -4,14 +4,19 @@ import type { Movie } from "../types/movie";
 
 export type MovieHttpResponse = {
   results: Movie[];
+  total_pages: number;
 };
 
-export default async function fetchMovies(query: string): Promise<Movie[]> {
+export default async function fetchMovies(
+  query: string,
+  page: number
+): Promise<MovieHttpResponse> {
   const response = await axios.get<MovieHttpResponse>(
     "https://api.themoviedb.org/3/search/movie",
     {
       params: {
         query: query,
+        page,
       },
       headers: {
         accept: "application/json",
@@ -19,5 +24,6 @@ export default async function fetchMovies(query: string): Promise<Movie[]> {
       },
     }
   );
-  return response.data.results;
+
+  return response.data;
 }
